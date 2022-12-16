ce5a2b68a3a6665534366f80a79a7f24bd8c0206
Joao-henrique-14 <113247777+Joao-henrique-14@users.noreply.github.com>
2022-12-16 12:11:49 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,23 +1,9 @@
-
- var numero= Number( prompt('digite um número'));
- var outro_numero= Number( prompt('digite outro número'));
- var operador= prompt('qual operador deseja utilizar? + - / *');
- var soma;
-if(operador== '+'){
-     soma= numero + outro_numero;
-     alert('o resultado da soma é ' + soma )}
-
-  else if(operador== '-'){
-     subtracao= numero - outro_numero;
-     alert('o resultado da subtração é ' + subtracao)}
-
-else if( operador== '/'){
-     divisao= numero / outro_numero;
-     alert('o resultado da divisão é ' + divisao)}
-
-else if(operador== '*'){
-     multiplicacao= numero * outro_numero;
-     alert('o resultado da multiplicação é ' +multiplicacao)}
-else{
-     alert('símbolo incorreto!(tente +, -, /, *')
+let saldo_inicial=100;
+let valor_compra=50;
+if(saldo_inicial >= valor_compra) {
+ alert (` a compra ocorreu corretamente`); 
+}else if(saldo_inicial <= valor_compra){
+    alert(`saldo insuficiente`);
 }
+
+
